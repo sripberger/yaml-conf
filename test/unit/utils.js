887b37f036dbe4cf23808decbee4f8a345132cd8
Steve Ripberger <steve.ripberger@zipscene.com>
2017-09-15 15:36:57 -0400

    Add utils

---
+++ test/unit/utils.js
@@ -0,0 +1,90 @@
+const utils = require('../../lib/utils');
+const fse = require('fs-extra');
+const os = require('os');
+const path = require('path');
+const yaml = require('js-yaml');
+const XError = require('xerror');
+
+describe('utils', function() {
+	describe('::getConfigPath', function() {
+		it('returns the full path to the configuration file', function() {
+			let file = 'path/to/config';
+			let expected = path.resolve(process.cwd(), file);
+
+			expect(utils.getConfigPath('foo', file)).to.deep.equal(expected);
+		});
+
+		it('supports absolute paths', function() {
+			let file = '/path/to/config';
+
+			expect(utils.getConfigPath('foo', file)).to.deep.equal(file);
+		});
+
+		it('defaults to app config in home folder', function() {
+			let expected = path.resolve(os.homedir(), 'foo.conf.yml');
+
+			expect(utils.getConfigPath('foo')).to.deep.equal(expected);
+		});
+	});
+
+	describe('::readConfig', function() {
+		const path = '/path/to/config';
+		const text = 'foo: bar';
+		const obj = { foo: 'bar' };
+
+		beforeEach(function() {
+			sandbox.stub(fse, 'readFileSync').returns(text);
+			sandbox.stub(yaml, 'safeLoad').returns(obj);
+		});
+
+		it('synchronously reads parsed yaml from provided path', function() {
+			let result = utils.readConfig(path);
+
+			expect(fse.readFileSync).to.be.calledOnce;
+			expect(fse.readFileSync).to.be.calledOn(fse);
+			expect(fse.readFileSync).to.be.calledWith(path, 'utf8');
+			expect(yaml.safeLoad).to.be.calledOnce;
+			expect(yaml.safeLoad).to.be.calledOn(yaml);
+			expect(yaml.safeLoad).to.be.calledWithExactly(text);
+			expect(result).to.deep.equal(obj);
+		});
+
+		it('throws invalid argument if file could not be read', function() {
+			let readErr = new Error('read error');
+			fse.readFileSync.throws(readErr);
+
+			expect(() => utils.readConfig(path))
+				.to.throw(XError).that.satisfies((err) => {
+					expect(err.code).to.equal(XError.INVALID_ARGUMENT);
+					expect(err.message).to.equal('Could not read config file.');
+					expect(err.data).to.deep.equal({ path });
+					expect(err.cause).to.equal(readErr);
+					return true;
+				});
+		});
+
+		it('returns empty object if allowFNF is true and file could not be read', function() {
+			let readErr = new Error('read error');
+			fse.readFileSync.throws(readErr);
+
+			let result = utils.readConfig(path, true);
+
+			expect(yaml.safeLoad).to.not.be.called;
+			expect(result).to.deep.equal({});
+		});
+
+		it('throws invalid argument if yaml parsing fails', function() {
+			let parseErr = new Error('parsing error');
+			yaml.safeLoad.throws(parseErr);
+
+			expect(() => utils.readConfig(path))
+				.to.throw(XError).that.satisfies((err) => {
+					expect(err.code).to.equal(XError.INVALID_ARGUMENT);
+					expect(err.message).to.equal('Invalid YAML in config file.');
+					expect(err.data).to.deep.equal({ path });
+					expect(err.cause).to.equal(parseErr);
+					return true;
+				});
+		});
+	});
+});
